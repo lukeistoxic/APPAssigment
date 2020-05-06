11f7c01304c6c7ee7b2f43530af6025f2c1dccf7
XPS15 <daniel_jones_008@hotmail.co.uk>
2020-05-06 13:40:40 +0100

    Ai now intelligent

--- lib/src/main/kotlin/uk/ac/bournemouth/ap/dotsandboxeslib/AIRandom.kt
+++ lib/src/main/kotlin/uk/ac/bournemouth/ap/dotsandboxeslib/AIRandom.kt
@@ -3,8 +3,21 @@ package uk.ac.bournemouth.ap.dotsandboxeslib
 open class AIRandom: ComputerPlayer (){
 
     override fun makeMove(game: DotsAndBoxesGame){
-        val possibleMoves = game.lines.filter { !it.isDrawn }
-        val chosenMove = possibleMoves.random()
-        chosenMove.drawLine()
+
+        var lineDrawn = false
+        for (box in game.boxes){
+            val lines = box.boundingLines.filter { !it.isDrawn }
+            if (lines.size == 1){
+                lines[0].drawLine()
+                lineDrawn = true
+                break
+
+            }
+        }
+        if(!lineDrawn) {
+            val possibleMoves = game.lines.filter { !it.isDrawn }
+            val chosenMove = possibleMoves.random()
+            chosenMove.drawLine()
+        }
     }
 }
